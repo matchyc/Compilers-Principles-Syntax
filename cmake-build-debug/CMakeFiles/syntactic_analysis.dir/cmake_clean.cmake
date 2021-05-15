6543a8bfff88f8932a95ef344f35ce364b005260
symphony233 <meng_chen99@163.com>
2021-05-15 16:42:52 +0800

    first commit
    Signed-off-by: symphony233 <meng_chen99@163.com>

---
+++ cmake-build-debug/CMakeFiles/syntactic_analysis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/syntactic_analysis.dir/main.cpp.obj"
+  "libsyntactic_analysis.dll.a"
+  "syntactic_analysis.exe"
+  "syntactic_analysis.exe.manifest"
+  "syntactic_analysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/syntactic_analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
